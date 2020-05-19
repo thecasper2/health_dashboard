7dede324d2415cf3ca972d8fc63c12913e506ecc
Alex Dolphin <trteam1988@gmail.com>
2020-05-19 19:42:35 +0200

    Refactor reading functions
    The reading, cleaning, and extending data actions have been turned into functions with a top-level function that chains all the functions together to return nice clean data!

--- health/read_scales_data.R
+++ health/read_scales_data.R
@@ -1,39 +1,123 @@
-scales_data <- fread("data/Renpho-Alex-data.csv")
+# This file provides a read_data() function, which:
+# - Reads in the .csv datasource in the /data subfolder
+# - Performs any necessary transformations on the data
+# - Returns the data as a data.table
 
-colnames(scales_data) <- c("time", "weight", "bmi", "body_fat",
-                           "fat_free_body_weight", "subcutaneous_fat",
-                           "visceral_fat", "body_water", "skeletal_muscle",
-                           "muscle_mass", "bone_mass", "protein", "bmr",
-                           "metabolic_age")
-remove_percent <- function(string){
-    return(as.numeric(str_remove(string, "%"))/100)
+library(data.table)
+library(lubridate)
+library(magrittr)
+library(stringr)
+
+# Define the columns we expect to find when reading the data
+expected_cols <- c("Time of Measurement", "Weight", "BMI", "Body Fat",
+                   "Fat-free Body Weight", "Subcutaneous Fat", "Visceral Fat",
+                   "Body Water", "Skeletal Muscle", "Muscle Mass", "Bone Mass",
+                   "Protein", "BMR", "Metabolic Age")
+
+# Define the columns that contain numeric data
+data_cols <- c("Weight", "BMI", "Body Fat", "Fat-free Body Weight",
+               "Subcutaneous Fat", "Visceral Fat", "Body Water",
+               "Skeletal Muscle", "Muscle Mass", "Bone Mass",
+               "Protein", "BMR", "Metabolic Age")
+
+scales_char_to_numeric <- function(string){
+
+    #' Takes a character value from the scales data, removes any description
+    #' of the unit (e.g. kgs, kcal, %) and returns the numeric value
+    #'
+    #' @param string the string to be turned into a numeric value
+
+    # Remove numeric values and decimal point to identify the character suffix
+    suffix <- str_remove_all(string, "[[[:digit:]]\\.]")
+    # If the suffix is unrecognised, throw an error
+    if(!(suffix %in% c("%", "kg", "kcal", ""))){
+        stop(paste0("Unrecognised character suffix: ", suffix))
+    }
+    # Remove suffix from string if it exists
+    if(suffix != ""){string <- str_remove_all(string, suffix)}
+    # Convert to numeric
+    numeric_out <- as.numeric(string)
+    # If value was a percent, return as a decimal
+    if(suffix == "%"){numeric_out <- numeric_out/100}
+    return(numeric_out)
 }
-remove_kg <- function(string){
-    return(as.numeric(str_remove(string, "kg")))
+
+read_scales_data <- function(directory="data", expected_cols_=expected_cols){
+
+    #' 1) Looks for any .csv files in the data directory, and reads in the first
+    #' instance.
+    #' 2) Checks whether the read data matches the expected columns.
+    #' 3) Returns data as a data.table
+    #'
+    #' @param directory The directory in which to look for data
+    #' @param expected_cols A vector of column names the data should have
+
+    # Find all .csv files in the specified directory
+    files <- list.files(directory, pattern="csv")
+    # Throw error if none are found
+    if(length(files) <= 0){stop(paste0("No .csv files found in /", directory))}
+    # Read first .csv file
+    data <- fread(paste0(directory, "/", files[1]))
+    # Check the column names are correct, if not throw an error.
+    if(!all(colnames(data) == expected_cols_)){
+        stop(
+            paste0(
+                "The following columns are missing in the data: ",
+                expected_cols[!(expected_cols %in% colnames(data))]
+            )
+        )
+    }
+    return(data)
 }
-remove_kcal <- function(string){
-    return(as.numeric(str_remove(string, "kcal")))
+
+clean_scales_data <- function(data, data_cols_=data_cols){
+
+    #' Takes the result of the read_scales_data() function and applies necessary
+    #' transformations for the dashboard.
+    #'
+    #' @param data The output of the read_scales_data() function
+    #' @param data_cols_w_suffix A vector of data columns that have a suffix,
+    #' e.g. kg
+
+    # Throw away rows with NA values, this can happen if the scales don't
+    # successfully read impedence data
+    data <- na.omit(data)
+
+    # Turn the time into a proper datetime format, and rename as "Time"
+    data[, `Time of Measurement` := as_datetime(
+        `Time of Measurement`,
+        format="%b %d, %Y %I:%M:%S %p"
+    )] %>% setnames(., old = "Time of Measurement", new = "Time")
+
+    # Remove suffixes from data columns and transform to numeric
+    data[,
+        (data_cols) := lapply(.SD, function(x) scales_char_to_numeric(x)),
+        .SDcols = data_cols_
+    ]
+    return(data[])
 }
-scales_data[, `:=` (
-    time = as_datetime(time, format="%b %d, %Y %I:%M:%S %p"),
-    weight = remove_kg(weight),
-    bmi = as.numeric(bmi),
-    body_fat = remove_percent(body_fat),
-    body_fat_percent = remove_percent(body_fat),
-    fat_free_body_weight = remove_kg(fat_free_body_weight),
-    subcutaneous_fat = remove_percent(subcutaneous_fat),
-    visceral_fat = as.numeric(visceral_fat),
-    body_water = remove_percent(body_water),
-    skeletal_muscle = remove_percent(skeletal_muscle),
-    muscle_mass = remove_kg(muscle_mass),
-    bone_mass = remove_kg(bone_mass),
-    protein = remove_percent(protein),
-    bmr = remove_kcal(bmr),
-    metabolic_age = as.numeric(metabolic_age)
-)]
-scales_data[, `:=` (
-    body_fat = weight*body_fat_percent,
-    muscle_mass_percent = muscle_mass / weight,
-    bone_mass_percent = bone_mass / weight
-)]
-melt_scales_data <- melt(scales_data, id="time")
+
+add_metrics <- function(data){
+
+    #' Takes cleaned scales data and adds additional metrics which are functions
+    #' of existing metrics
+    #'
+    #' @param data The output of clean_scales_data()
+
+    data[, `:=` (
+        `Body Fat kg` = `Body Fat` * Weight,
+        `Muscle Mass %` = `Muscle Mass` / Weight,
+        `Bone Mass %` = `Bone Mass` / Weight
+    )]
+
+    return(data[])
+}
+
+get_scales_data <- function(){
+
+    #' Reads the scales data, applys the cleaning function, adds metrics,
+    #' then returns the resulting data.table
+
+    data <- read_scales_data() %>% clean_scales_data() %>% add_metrics()
+    return(data)
+}
